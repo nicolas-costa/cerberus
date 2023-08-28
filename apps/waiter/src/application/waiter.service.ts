9cbb8fa7e5f8a319f5ec6c6085402f81d9eea272
Nícolas Costa <nicolas.nico@hotmail.com>
2023-08-27 23:38:05 -0300

    feat: update module imports

--- apps/waiter/src/application/waiter.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class WaiterService {
-  getHello(): string {
-    return 'Restaurant menu page';
-  }
-}
--- apps/waiter/src/waiter.module.ts
+++ apps/waiter/src/waiter.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { WaiterController } from './infrastructure/waiter.controller';
-import { WaiterService } from './application/waiter.service';
+import { RestaurantController } from './infrastructure/restaurant.controller';
+import { HomeService } from './application/home.service';
+import {RedisChartRepository} from "./infrastructure/redis-chart.repository";
+import ChartPort from "./application/ports/chart.port";
 
 @Module({
-  imports: [],
-  controllers: [WaiterController],
-  providers: [WaiterService],
+  imports: [
+  ],
+  controllers: [RestaurantController],
+  providers: [
+    HomeService, {
+      provide: ChartPort,
+      useClass: RedisChartRepository
+    }],
 })
 export class WaiterModule {}
